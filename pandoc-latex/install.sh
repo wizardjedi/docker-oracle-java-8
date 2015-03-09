b0251cf6cf668ae8f09e7e394b0258f6adc60cd3
Sayapin Alexander <wizardjedi@gmail.com>
2015-03-09 09:18:57 +0300

    + Added Docker file for pandoc+tex image

---
+++ pandoc-latex/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update 
+apt-get install -y texlive texlive-full pandoc
+
+apt-get -y autoclean
+apt-get -y clean 
+apt-get -y purge 
+apt-get -y autoremove
+
+rm -rf /var/lib/apt/lists/* /usr/share/doc/* /usr/share/doc-base/* /usr/share/zoneinfo/* /usr/share/locale/* /usr/share/man/* 
+
