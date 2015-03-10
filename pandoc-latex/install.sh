607adab0e468865aa3260b711a855642cafb8662
Sayapin Alexander <wizardjedi@gmail.com>
2015-03-10 22:20:48 +0300

    + Added installing fresh version (1.13.2) of pandoc

--- pandoc-latex/install.sh
+++ pandoc-latex/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 apt-get update 
-apt-get install -y texlive texlive-full pandoc gpp
+apt-get install -y wget libgmp10
+cd tmp
+wget --no-check-certificate https://github.com/jgm/pandoc/releases/download/1.13.2/pandoc-1.13.2-1-amd64.deb
+dpkg -i pandoc-1.13.2-1-amd64.deb
+
+apt-get install -y texlive texlive-full gpp
 
 apt-get -y autoclean
 apt-get -y clean 
