8154a75393ae2ca578fc9865bb8ba221e79ecb40
Sayapin Alexander <wizardjedi@gmail.com>
2015-05-26 22:37:30 +0300

    + Added minimal erlang + build-essentials

---
+++ erlang/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y wget build-essential autoconf libtool
+
+cd tmp
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+dpkg -i erlang-solutions_1.0_all.deb
+
+apt-get update
+apt-get install -y erlang-nox erlang-dev erlang-base-hipe
+
+rm erlang-solutions_1.0_all.deb
+
+apt-get -y autoclean
+apt-get -y clean 
+apt-get -y purge 
+apt-get -y autoremove
+
+rm -rf /var/lib/apt/lists/* /usr/share/doc/* /usr/share/doc-base/* /usr/share/zoneinfo/* /usr/share/locale/* /usr/share/man/* 
+
