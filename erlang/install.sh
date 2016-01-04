60de23534d8fc8c623988e198795cb5e5dc213d4
Sayapin Alexander <wizardjedi@gmail.com>
2016-01-04 11:03:07 +0300

    * New version of erlang

--- erlang/install.sh
+++ erlang/install.sh
@@ -8,6 +8,8 @@ wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 dpkg -i erlang-solutions_1.0_all.deb
 
 apt-get update
+
+# Install erlang 18.2.1
 apt-get install -y erlang-nox erlang-dev erlang-base-hipe
 
 rm erlang-solutions_1.0_all.deb
